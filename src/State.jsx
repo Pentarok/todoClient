2c6c3d7403e64265ceab67ac5fb97e6582869be7
Pentarok <makpentarok@gmail.com>
2024-09-24 13:38:14 +0300

    seventh

--- src/State.jsx
+++ src/State.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { ThreeDots } from 'react-loader-spinner'; // Import the ThreeDots loader
 import './state.css';
+
 const TodoApp = () => {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
@@ -44,77 +45,92 @@ const TodoApp = () => {
     }
   };
 
-  const addTask = async () => {
+  const addTask = () => {
     if (newTask.trim() !== '') {
-      try {
-        const res = await axios.post(`${serverUri}/todo`, { newTask }, { withCredentials: true });
-        const newTaskObject = {
-          _id: res.data._id,
-          Task: res.data.Task,
-          createdAt: res.data.createdAt,
-          done: res.data.done,
-        };
-        setTasks((t) => [...t, newTaskObject]);
-        setNewTask('');
-      } catch (error) {
+      const newTaskObject = {
+        _id: Date.now().toString(), // Temporary ID for fast UI updates
+        Task: newTask,
+        createdAt: new Date(),
+        done: false,
+      };
+      setTasks((prevTasks) => [...prevTasks, newTaskObject]);
+      setNewTask('');
+
+      // Update the server in the background
+      axios.post(`${serverUri}/todo`, { newTask }, { withCredentials: true })
+        .then((res) => {
+          // Replace temporary task with task from server
+          setTasks((prevTasks) =>
+            prevTasks.map((task) =>
+              task._id === newTaskObject._id ? { ...task, _id: res.data._id } : task
+            )
+          );
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  };
+
+  const deleteTask = (index, id) => {
+    // Remove task from UI first
+    setTasks((prevTasks) => prevTasks.filter((_, i) => i !== index));
+
+    // Update the server in the background
+    axios.delete(`${serverUri}/todo/${id}`)
+      .catch((error) => {
         console.error(error);
-      }
-    }
+      });
   };
 
-  const deleteTask = async (index, id) => {
-    try {
-      await axios.delete(`${serverUri}/todo/${id}`);
-      setTasks(tasks.filter((_, i) => i !== index));
-    } catch (error) {
-      console.error(error);
-    }
+  const markAsDone = (id, done) => {
+    // Optimistically update the UI
+    setTasks((prevTasks) =>
+      prevTasks.map((task) => (task._id === id ? { ...task, done: !done } : task))
+    );
+
+    // Update the server in the background
+    axios.put(`${serverUri}/todo/${id}/markDone`, { done: !done })
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
-  const markAsDone = async (id, done) => {
-    try {
-      const res = await axios.put(`${serverUri}/todo/${id}/markDone`, { done: !done });
-      setTasks(tasks.map((task) => (task._id === id ? res.data : task)));
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const moveTaskUp = async (index) => {
+  const moveTaskUp = (index) => {
     if (index > 0) {
       const updatedTasks = [...tasks];
       [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
       setTasks(updatedTasks);
-      await updateTaskOrderInDB(updatedTasks);
+      updateTaskOrderInDB(updatedTasks);
     }
   };
 
-  const moveTaskDown = async (index) => {
+  const moveTaskDown = (index) => {
     if (index < tasks.length - 1) {
       const updatedTasks = [...tasks];
       [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
       setTasks(updatedTasks);
-      await updateTaskOrderInDB(updatedTasks);
+      updateTaskOrderInDB(updatedTasks);
     }
   };
 
-  const updateTaskOrderInDB = async (updatedTasks) => {
-    try {
-      const tasksToUpdate = updatedTasks.map((task, index) => ({
-        _id: task._id,
-        order: index,
-      }));
+  const updateTaskOrderInDB = (updatedTasks) => {
+    const tasksToUpdate = updatedTasks.map((task, index) => ({
+      _id: task._id,
+      order: index,
+    }));
 
-      await axios.put(`${serverUri}/todo/updateOrder`, { tasks: tasksToUpdate }, { withCredentials: true });
-    } catch (error) {
-      console.error("Error updating task order:", error);
-    }
+    // Update the server in the background
+    axios.put(`${serverUri}/todo/updateOrder`, { tasks: tasksToUpdate }, { withCredentials: true })
+      .catch((error) => {
+        console.error("Error updating task order:", error);
+      });
   };
 
   if (loading) {
     return (
       <div className="loader">
-        <div><ThreeDots
+        <ThreeDots
           visible={true}
           height="80"
           width="80"
@@ -124,7 +140,6 @@ const TodoApp = () => {
           wrapperStyle={{}}
           wrapperClass=""
         />
-        </div>
       </div>
     );
   }
