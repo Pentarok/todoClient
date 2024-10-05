cfcfda57024055f89ebceabc7328f399eb5a38c5
Pentarok <167968478+Pentarok@users.noreply.github.com>
2024-10-05 18:58:44 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import TodoApp from './State.jsx';
-import BlogList from './BlogList'; // Example import for BlogList component
-import About from './About'; // Example import for About component
-import Contact from './Contact'; // Example import for Contact component
+import TodoApp from './Todo.jsx';
+
 import NotFound from './NotFound'; // A new NotFound component for undefined routes
 
 function App() {
@@ -11,10 +9,9 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<BlogList />} />
-          <Route path='/about' element={<About />} />
+        
           <Route path='/todo' element={<TodoApp />} />
-          <Route path='/contact' element={<Contact />} />
+
           {/* Catch-all route for undefined paths */}
           <Route path='*' element={<NotFound />} />
         </Routes>
