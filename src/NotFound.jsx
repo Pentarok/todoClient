94921e949d332f68e6ccc40fe128897970eaab81
Pentarok <167968478+Pentarok@users.noreply.github.com>
2024-10-05 18:54:43 +0300

    Create NotFound.jsx

---
+++ src/NotFound.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div style={{display:'flex',justifyContent:'center',alignItems:'center', width:'100%'}}>
+    <div>
+
+    <h1>404 - Page Not Found</h1>
+    <p>Oops! The page you're looking for doesn't exist.</p>
+        </div>
+    </div>
+  );
+}
+
+export default NotFound;
